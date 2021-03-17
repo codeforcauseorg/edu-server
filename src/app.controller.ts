f0fe2af602aa30d447a1380881b5adf6aa759a87
NeelParihar <neelparihar599@gmail.com>
2021-03-17 17:59:24 +0530

    added swagger ui and configured it

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,20 @@
 import { Controller, Get, Inject, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { FirebaseAdminSDK, FIREBASE_ADMIN_INJECT } from '@tfarras/nestjs-firebase-admin';
+import {
+  FirebaseAdminSDK,
+  FIREBASE_ADMIN_INJECT,
+} from '@tfarras/nestjs-firebase-admin';
 import { AppService } from './app.service';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
+@ApiBearerAuth()
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     @Inject(FIREBASE_ADMIN_INJECT) private readonly fireSDK: FirebaseAdminSDK,
-  ) { }
- 
+  ) {}
+
   @Get()
   @UseGuards(AuthGuard('firebase'))
   getHello() {
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,46 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerCustomOptions,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // configure swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Edu Server')
+    .setDescription('Edu Server API documentation')
+    .setVersion('0.0.1')
+    .setContact(
+      'Code for Cause',
+      'https://codeforcause.org/',
+      'team@codeforcause.org',
+    )
+    .setLicense(
+      'MIT',
+      'https://github.com/codeforcauseorg/edu-server/blob/master/LICENSE',
+    )
+    .addServer('http://localhost:5000/', 'Development Server')
+    .addBearerAuth()
+    .build();
+
+  // adding custom options
+  const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+    customCss: '.swagger-ui .topbar { display: none }',
+    customSiteTitle: 'Edu Server API Docs',
+    customfavIcon: 'https://codeforcause.org/favicon.ico',
+  };
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, document, customOptions);
+
   await app.listen(5000);
-  console.log("App is listening on port: 5000");
+  console.log('App is listening on port: 5000');
 }
 
 bootstrap();
