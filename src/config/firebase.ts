5fe77f63ccfd795ecdf459e904c4db4362159a37
Devesh <rockdev1234560@gmail.com>
2021-03-18 11:11:53 +0530

    removed unneccessaryy config export files

--- src/config/firebase.ts
+++
@@ -1,9 +0,0 @@
-import admin from 'firebase-admin';
-
-import * as firebaseAccountCredentials from './service.json';
-
-const serviceAccount = firebaseAccountCredentials as admin.ServiceAccount;
-
-export default {
-  credential: admin.credential.cert(serviceAccount),
-};
