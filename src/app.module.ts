a92ae89b2091c2c01bcdb556a04dcfdca96640b2
Nishi Agrawal <nishihere19@gmail.com>
2021-03-20 19:42:21 +0530

    minor change, removal of unused var configservice from app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from 'nestjs-config';
+import { ConfigModule } from 'nestjs-config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
