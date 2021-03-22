c93d6765bcd8879d84695e9fa7ba31d83a363602
Devesh Kumar <59202075+Devesh21700Kumar@users.noreply.github.com>
2021-03-23 01:44:22 +0530

    connectec nest js with mongoose and initialised the server with compl… (#29)
    Co-authored-by: Abhishek Kumar <abhimait1909@gmail.com>
    Co-authored-by: souvik <souvikde.ns@gmail.com>

---
+++ src/user/user.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './interfaces/user.interface';
+import { CreateUserDTO } from './dto/create-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(@InjectModel('User') private readonly UserModel: Model<User>) {}
+
+  // fetch all Users
+  async getAllUser(): Promise<User[]> {
+    const Users = await this.UserModel.find().exec();
+    return Users;
+  }
+
+  // Get a single User
+  async getUser(UserID): Promise<User> {
+    const User = await this.UserModel.findById(UserID).exec();
+    return User;
+  }
+
+  // post a single User
+  async addUser(CreateUserDTO: CreateUserDTO): Promise<User> {
+    const newUser = await new this.UserModel(CreateUserDTO);
+    return newUser.save();
+  }
+
+  // Edit User details
+  async updateUser(UserID, CreateUserDTO: CreateUserDTO): Promise<User> {
+    const updatedUser = await this.UserModel.findByIdAndUpdate(
+      UserID,
+      CreateUserDTO,
+      { new: true },
+    );
+    return updatedUser;
+  }
+
+  // Delete a User
+  async deleteUser(UserID): Promise<any> {
+    const deletedUser = await this.UserModel.findByIdAndRemove(UserID);
+    return deletedUser;
+  }
+}
