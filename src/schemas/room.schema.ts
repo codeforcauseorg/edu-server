e632edaea3a72261311ff275357cd5467392572e
Devesh <rockdev1234560@gmail.com>
2021-05-12 00:37:45 +0530

    add schema for chatroom that will be needed

---
+++ src/schemas/room.schema.ts
@@ -0,0 +1,23 @@
+import { Chat } from './chat.schema';
+import { User } from './user.schema';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+
+export type RoomDocument = Room & Document;
+
+@Schema()
+export class Room {
+  @Prop()
+  id: string;
+
+  @Prop({ required: true, maxlength: 20, minlength: 5 })
+  name: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  chats: Chat;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  connectedUsers: User;
+}
+
+export const RoomSchema = SchemaFactory.createForClass(Room);
