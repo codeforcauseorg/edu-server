eb73448b20d883eba846aea1f673b0f476a3fa73
Devesh <rockdev1234560@gmail.com>
2021-05-12 16:34:52 +0530

    add document import to avoid bug of missing socument

--- src/schemas/room.schema.ts
+++ src/schemas/room.schema.ts
@@ -2,6 +2,7 @@ import { Chat } from './chat.schema';
 import { User } from './user.schema';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
+import { Document } from 'mongoose';
 
 export type RoomDocument = Room & Document;
 
