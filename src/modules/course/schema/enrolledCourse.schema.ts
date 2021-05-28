70b2a0b047b4bf640d392cf77469acecb847b499
Devesh <rockdev1234560@gmail.com>
2021-05-28 23:11:05 +0530

    fixed schema

--- src/modules/course/schema/enrolledCourse.schema.ts
+++ src/modules/course/schema/enrolledCourse.schema.ts
@@ -3,46 +3,21 @@ import { Document } from 'mongoose';
 
 export type EnrolledCourseDocument = EnrolledCourse & Document;
 
+interface video {
+  num: number;
+  timestamp?: Date;
+}
+
 @Schema()
 export class EnrolledCourse {
-  @Prop({ required: true })
-  name: string;
+  @Prop({ required: true, default: [] })
+  videos_watched: string[];
 
-  @Prop({ required: true })
-  price: number;
+  @Prop({ required: true, default: [] })
+  Assignments_done: string[];
 
-  @Prop({ required: true, default: Date.now })
-  start_date: Date;
-
-  @Prop()
-  end_date: Date;
-
-  @Prop()
-  duration: string;
-
-  @Prop({ default: false })
-  active: boolean;
-
-  @Prop()
-  coupons: number;
-
-  @Prop({ default: 0 })
-  student_num: number;
-
-  @Prop()
-  mentor: [];
-
-  @Prop()
-  video_num: number;
-
-  @Prop()
-  assignments: string[]; //links to questions pdf
-
-  @Prop({ default: 0 })
-  no_of_enrollments: number;
-
-  @Prop()
-  sharable_link: string;
+  @Prop({ required: true, default: [] })
+  currentVideo: video[];
 }
 
 export const EnrolledCourseSchema = SchemaFactory.createForClass(
