6bda1764c4570058e9d2c2f5e9a2207fb24fe9d9
Devesh <rockdev1234560@gmail.com>
2021-06-03 09:55:28 +0530

    swagger docs and ApiProperty for create user dto

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -5,30 +5,63 @@ export class CreateUserDTO {
   @IsNotEmpty()
   @ApiProperty({
     type: String,
-    description: 'The name of the student',
+    description: 'The first name of the student',
     default: '',
   })
   readonly first_name: string;
 
   @ApiProperty({
     type: String,
-    description: 'The name of the student',
+    description: 'The last name of the student',
     default: '',
   })
   readonly last_name: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    description: 'The email of the student',
+    default: '',
+  })
   readonly email: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    type: String,
+    description: 'The phone number of the student',
+    default: '',
+  })
   readonly phone: string;
 
+  @ApiProperty({
+    type: String,
+    description: 'The address of the student',
+    default: '',
+  })
   readonly address: string;
 
+  @ApiProperty({
+    type: String,
+    description: 'The description of the student',
+    default: '',
+  })
   readonly description: string;
 
+  @ApiProperty({
+    type: Number,
+    description: 'The score of the student',
+  })
   readonly score: number;
+
+  @ApiProperty({
+    type: Boolean,
+    description: 'The field to show of whether the students is Admin or not',
+  })
   readonly isAdmin: boolean;
 
+  @ApiProperty({
+    type: [String],
+    description: 'The list of wishlisted courses',
+  })
   readonly wishlist: string[];
 }
