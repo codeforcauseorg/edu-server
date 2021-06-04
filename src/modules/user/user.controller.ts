020b7474ec5de3c0a18824c4ce32d7d8ba74f9a3
Devesh <rockdev1234560@gmail.com>
2021-06-04 08:55:18 +0530

    moved UserResponseBody to different folder

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -8,43 +8,12 @@ import {
   Put,
 } from '@nestjs/common';
 import * as mongoose from 'mongoose';
-import { ApiCreatedResponse, ApiProperty } from '@nestjs/swagger';
+import { UserResponseBody } from './user.responseBody';
+import { ApiCreatedResponse } from '@nestjs/swagger';
 import { CreateUserDTO } from './dto/create-user.dto'; //eslint-disable-line 
 import { UpdateUserDTO } from './dto/update-user.dto'; //eslint-disable-line 
 import { UserService } from './user.service'; //eslint-disable-line 
 import { CreateEnrolledDTO } from './dto/create-enrolled.dto';
-
-class UserResponseBody {
-  @ApiProperty({ required: true, example: '605e3fd9acc33583fb389aec' })
-  _id: string;
-
-  @ApiProperty({ required: true, example: 'Noob' })
-  first_name: string;
-
-  @ApiProperty({ required: true, example: 'Coder' })
-  last_name: string;
-
-  @ApiProperty({ required: true, example: 'noobcoder@gmai.com' })
-  email: string;
-
-  @ApiProperty({ required: true, example: '+919999999999' })
-  phone: string;
-
-  @ApiProperty({ required: true, example: 'A-88, Mayur Vihar, Delhi' })
-  address: string;
-
-  @ApiProperty({ required: true, example: 'I am Noob Coder' })
-  description: string;
-
-  @ApiProperty({ required: false, example: 80 })
-  score: number;
-
-  @ApiProperty({ required: true, example: true })
-  isAdmin: boolean;
-
-  @ApiProperty({ required: false, example: ['DSA', 'Baka'] })
-  wishlist: string[];
-}
 
 @Controller('user')
 export class UserController {
---
+++ src/modules/user/user.responseBody.ts
@@ -0,0 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UserResponseBody {
+  @ApiProperty({ required: true, example: '605e3fd9acc33583fb389aec' })
+  _id: string;
+
+  @ApiProperty({ required: true, example: 'Noob' })
+  first_name: string;
+
+  @ApiProperty({ required: true, example: 'Coder' })
+  last_name: string;
+
+  @ApiProperty({ required: true, example: 'noobcoder@gmai.com' })
+  email: string;
+
+  @ApiProperty({ required: true, example: '+919999999999' })
+  phone: string;
+
+  @ApiProperty({ required: true, example: 'A-88, Mayur Vihar, Delhi' })
+  address: string;
+
+  @ApiProperty({ required: true, example: 'I am Noob Coder' })
+  description: string;
+
+  @ApiProperty({ required: false, example: 80 })
+  score: number;
+
+  @ApiProperty({ required: true, example: true })
+  isAdmin: boolean;
+
+  @ApiProperty({ required: false, example: ['DSA', 'Baka'] })
+  wishlist: string[];
+}
