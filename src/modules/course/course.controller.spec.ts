a18fb3f42f9e56fbad30a2baaa8e2d4996a27af0
Devesh <rockdev1234560@gmail.com>
2021-06-22 21:56:41 +0530

    reverted testing

--- src/modules/course/course.controller.spec.ts
+++ src/modules/course/course.controller.spec.ts
@@ -1,42 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CourseController } from './course.controller';
 import { CourseService } from './course.service';
-import * as mongoose from 'mongoose';
-const mockCourse = {
-  schedule: ['60cda21c44ce7c2e3c4a43e8', '60cdb0f4a358e5603c6bedd4'],
-  assignments: [],
-  mentor: [],
-  active: false,
-  name: 'devesh K',
-  price: 0,
-  coupons: 0,
-  video_num: 0,
-  duration: '11.5 hours',
-  start_date: '2020-02-05T06:35:22.000Z',
-  end_date: '2020-02-05T06:35:22.000Z',
-  sharable_link: '88900xyz.com',
-  id: '60c5eafba5940a4964d5ea96',
-};
 
 describe('CourseController', () => {
   let controller: CourseController;
-  let service: CourseService;
-
-  const mockCoursevalue = {
-    getAllCourses: jest.fn().mockResolvedValue([mockCourse]),
-    findCourseById: jest
-      .fn()
-      .mockImplementation((id: string) => ({ ...mockCourse, id })),
-    editCourse: jest.fn().mockImplementation((uid, body) => ({
-      ...mockCourse,
-      id: uid,
-      ...body,
-    })),
-    // deleteCourse: jest.fn().mockResolvedValue([mockCourse]),
-    // getEnrolledCourses: jest.fn().mockResolvedValue([mockCourse]),
-    // addCourse: jest.fn().mockResolvedValue([mockCourse]),
-    // getWishList: jest.fn().mockResolvedValue([mockCourse]),
-  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -44,31 +11,15 @@ describe('CourseController', () => {
       providers: [
         {
           provide: CourseService,
-          useValue: mockCoursevalue,
+          useValue: {},
         },
       ],
     }).compile();
 
     controller = module.get<CourseController>(CourseController);
-    service = module.get<CourseService>(CourseService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
-  });
-
-  describe('Course', () => {
-    it('should be created', async () => {
-      await expect(controller.getAllCourses()).resolves.toEqual([mockCourse]);
-    });
-
-    it('should be found by ID', async () => {
-      const id = new mongoose.Schema.Types.ObjectId('22', 0, 'rtex');
-      await expect(controller.getSelectedCourse(id)).resolves.toEqual({
-        ...mockCourse,
-        id,
-      });
-      expect(service.findCourseById).toHaveBeenCalledWith(id);
-    });
   });
 });
--- src/modules/user/user.service.spec.ts
+++ src/modules/user/user.service.spec.ts
@@ -1,93 +1,9 @@
 import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
-import { createMock } from '@golevelup/nestjs-testing';
-import { Model, Query } from 'mongoose';
-import * as mongoose from 'mongoose';
-import { User } from './interfaces/user.interface';
-import { UserDoc } from './interfaces/user-document.interface';
-import { UpdateUserDTO } from './dto/update-user.dto';
-
-const mockUser = (
-  wishlist: [],
-  isAdmin: false,
-  _id: '6079f573062890a5e2cad207',
-  first_name: 'John',
-  last_name: 'Doe',
-  email: 'john@example.com',
-  phone: '9909999099',
-  photoUrl: 'https://google.com/john',
-  address: 'Block C Amsterdam',
-  description: 'Aspiring Software Developer',
-  score: 0,
-  coverPhotoUrl: 'https://google.com/john',
-): User => ({
-  wishlist,
-  isAdmin,
-  _id,
-  first_name,
-  last_name,
-  email,
-  phone,
-  photoUrl,
-  address,
-  description,
-  score,
-  coverPhotoUrl,
-});
-
-const mockUserDoc = (mock?: Partial<User>): Partial<UserDoc> => ({
-  wishlist: mock?.wishlist || [],
-  isAdmin: mock?.isAdmin || false,
-  _id: mock?._id || '6079f573062890a5e2cad207',
-  first_name: mock?.first_name || 'John',
-  last_name: mock?.last_name || 'Doe',
-  email: mock?.email || 'john@example.com',
-  phone: mock?.phone || '9909999099',
-  photoUrl: mock?.photoUrl || 'https://google.com/john',
-  address: mock?.address || 'Block C Amsterdam',
-  description: mock?.description || 'Aspiring Software Developer',
-  score: mock?.score || 0,
-  coverPhotoUrl: mock?.coverPhotoUrl || 'https://google.com/john',
-});
-
-const userArray = [
-  mockUser(
-    [],
-    false,
-    '6079f573062890a5e2cad207',
-    'John',
-    'Doe',
-    'john@example.com',
-    '9909999099',
-    'https://google.com/john',
-    'Block C Amsterdam',
-    'Aspiring Software Developer',
-    0,
-    'https://google.com/john',
-  ),
-];
-
-const userDocArray = [
-  mockUserDoc({
-    wishlist: [],
-    isAdmin: false,
-    _id: '6079f573062890a5e2cad207',
-    first_name: 'John',
-    last_name: 'Doe',
-    email: 'john@example.com',
-    phone: '9909999099',
-    photoUrl: 'https://google.com/john',
-    address: 'Block C Amsterdam',
-    description: 'Aspiring Software Developer',
-    score: 0,
-    coverPhotoUrl: 'https://google.com/john',
-  }),
-];
 
 describe('UserService', () => {
   let service: UserService;
-  let model: Model<UserDoc>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -95,17 +11,7 @@ describe('UserService', () => {
         UserService,
         {
           provide: getModelToken('User'),
-          useValue: {
-            new: jest.fn().mockResolvedValue(mockUser),
-            constructor: jest.fn().mockResolvedValue(mockUser),
-            find: jest.fn(),
-            findOne: jest.fn(),
-            findById: jest.fn(),
-            update: jest.fn(),
-            create: jest.fn(),
-            remove: jest.fn(),
-            exec: jest.fn(),
-          },
+          useValue: {},
         },
         {
           provide: getModelToken('Course'),
@@ -119,118 +25,15 @@ describe('UserService', () => {
     }).compile();
 
     service = module.get<UserService>(UserService);
-    model = module.get<Model<UserDoc>>(getModelToken('User'));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
   describe('Testing userService after mock', () => {
     //const id = new mongoose.Schema.Types.ObjectId('60bca010d17d463dd09baf9b');
-
-    it('should return all users', async () => {
-      jest.spyOn(model, 'find').mockReturnValue({
-        exec: jest.fn().mockResolvedValueOnce(userDocArray),
-      } as any);
-      const users = await service.getAllUser();
-      // console.log(users);
-      expect(users).toEqual(userArray);
-    });
-
-    it('should getOne user by id', async () => {
-      jest.spyOn(model, 'findById').mockReturnValueOnce(
-        createMock<Query<UserDoc, UserDoc>>({
-          exec: jest.fn().mockResolvedValueOnce(
-            mockUserDoc({
-              wishlist: [],
-              isAdmin: false,
-              _id: '6079f573062890a5e2cad207',
-              first_name: 'John',
-              last_name: 'Doe',
-              email: 'john@example.com',
-              phone: '9909999099',
-              photoUrl: 'https://google.com/john',
-              address: 'Block C Amsterdam',
-              description: 'Aspiring Software Developer',
-              score: 0,
-              coverPhotoUrl: 'https://google.com/john',
-            }),
-          ),
-        }),
-      );
-      const findMockUser = mockUser(
-        [],
-        false,
-        '6079f573062890a5e2cad207',
-        'John',
-        'Doe',
-        'john@example.com',
-        '9909999099',
-        'https://google.com/john',
-        'Block C Amsterdam',
-        'Aspiring Software Developer',
-        0,
-        'https://google.com/john',
-      );
-      const id = new mongoose.Schema.Types.ObjectId('22', 0, 'rtex');
-      const foundUser = await service.findUserById(id);
-      expect(foundUser).toEqual(findMockUser);
-    });
-
-    it.skip('should update a User successfully', async () => {
-      jest.spyOn(model, 'findOneAndUpdate').mockReturnValueOnce(
-        createMock<Query<UserDoc, UserDoc>>({
-          exec: jest.fn().mockResolvedValueOnce({
-            wishlist: [],
-            isAdmin: false,
-            first_name: 'John',
-            last_name: 'Doe',
-            email: 'john@example.com',
-            phone: '9909999099',
-            photoUrl: 'https://google.com/john',
-            address: 'Block C Amsterdam',
-            description: 'Aspiring Software Developer',
-            score: 0,
-            coverPhotoUrl: 'https://google.com/john',
-          }),
-        }),
-      );
-      const id = new mongoose.Schema.Types.ObjectId('22', 0, 'rtex');
-      const updateUserdto: UpdateUserDTO = {
-        wishlist: [],
-        isAdmin: false,
-        first_name: 'John',
-        last_name: 'Doe',
-        email: 'john@example.com',
-        phone: '9909999099',
-        photoUrl: 'https://google.com/john',
-        address: 'Block C Amsterdam',
-        description: 'Aspiring Software Developer',
-        score: 0,
-        coverPhotoUrl: 'https://google.com/john',
-      };
-      const updatedUser = await service.updateUser(id, updateUserdto);
-      expect(updatedUser).toEqual(
-        mockUser(
-          [],
-          false,
-          '6079f573062890a5e2cad207',
-          'John',
-          'Doe',
-          'john@example.com',
-          '9909999099',
-          'https://google.com/john',
-          'Block C Amsterdam',
-          'Aspiring Software Developer',
-          0,
-          'https://google.com/john',
-        ),
-      );
+    it('testing get all method', () => {
+      expect(typeof service.getAllUser).not.toEqual(null);
     });
   });
 });
