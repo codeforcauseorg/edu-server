59b589565729c60973ab0cd268676251993c3746
Devesh <rockdev1234560@gmail.com>
2021-07-04 10:12:40 +0530

    added cors middleware and helmet in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 import { swaggerConfig, customOptions } from './swagger-ui';
 import { SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-
+import * as helmet from 'helmet';
+import { Request, Response, NextFunction } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -12,6 +13,19 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document, customOptions);
 
   app.enableCors();
+  app.use(helmet());
+
+  // The Cors handling middleware
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Headers', '*');
+    res.set('Access-Control-Allow-Methods', '*');
+    if (req.method === 'OPTIONS') {
+      res.status(200).end();
+      return;
+    }
+    next();
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(5000);
   console.log('App is listening on port: 5000');
