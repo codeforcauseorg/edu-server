44b0a446f4f7d1b3bf7bc506ef0e59728eb3b981
Abhishek-kumar09 <abhimait1909@gmail.com>
2021-07-16 14:24:27 +0530

    fix production deployment issues for heroku

--- src/config/service.ts
+++ src/config/service.ts
@@ -3,7 +3,7 @@ const config = {
     "type": process.env.TYPE,
     "project_id": process.env.PROJECT_ID,
     "private_key_id": process.env.PRIVATE_KEY_ID,
-    "private_key": process.env.PRIVATE_KEY,
+    "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n') ,
     "client_email": process.env.CLIENT_EMAIL,
     "client_id": process.env.CLIENT_ID,
     "auth_uri": process.env.AUTH_URI,
@@ -11,11 +11,11 @@ const config = {
     "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
     "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
   },
-  prod: {
+  production: {
     "type": process.env.TYPE,
     "project_id": process.env.PROJECT_ID,
     "private_key_id": process.env.PRIVATE_KEY_ID,
-    "private_key": process.env.PRIVATE_KEY,
+    "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n') ,
     "client_email": process.env.CLIENT_EMAIL,
     "client_id": process.env.CLIENT_ID,
     "auth_uri": process.env.AUTH_URI,
