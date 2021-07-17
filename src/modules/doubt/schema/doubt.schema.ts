bd3ae21cac7e2e767ece014a379fa5eb94800e33
Devesh <rockdev1234560@gmail.com>
2021-07-17 18:31:18 +0530

    remove * and added Document import instead

--- src/modules/doubt/schema/doubt.schema.ts
+++ src/modules/doubt/schema/doubt.schema.ts
@@ -1,10 +1,10 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import * as mongoose from 'mongoose';
 import { Schema as SchemaType } from 'mongoose';
 import { TagType } from '../doubt-tag.enum';
 import { DoubtAnswer } from './doubtAnswer.schema';
+import { Document } from 'mongoose';
 
-export type DoubtDocument = Doubt & mongoose.Document;
+export type DoubtDocument = Doubt & Document;
 
 @Schema()
 export class Doubt {
--- src/modules/doubt/schema/doubtAnswer.schema.ts
+++ src/modules/doubt/schema/doubtAnswer.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import * as mongoose from 'mongoose';
+import { Document } from 'mongoose';
 
-export type DoubtAnswerDocument = DoubtAnswer & mongoose.Document;
+export type DoubtAnswerDocument = DoubtAnswer & Document;
 
 @Schema()
 export class DoubtAnswer {
