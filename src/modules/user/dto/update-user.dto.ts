94f2fc22c6143e51ddcbf16cafc422cdd18b7094
Devesh <rockdev1234560@gmail.com>
2021-07-22 16:44:09 +0530

    remove unused IsString

--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsNotEmpty, IsString } from 'class-validator';
+import { IsArray, IsNotEmpty } from 'class-validator';
 import { Schema } from 'mongoose';
 
 export class UpdateUserDTO {
