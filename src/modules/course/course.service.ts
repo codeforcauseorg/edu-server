9443bd4ce6eee4a8e84df7f0729bc6c7fbb4ec09
Abhishek-kumar09 <abhimait1909@gmail.com>
2021-07-24 18:45:06 +0530

    fix typo in review

--- src/modules/course/course.service.ts
+++ src/modules/course/course.service.ts
@@ -54,7 +54,7 @@ export class CourseService {
         .select(
           'name courseShortDescription tags rating no_of_enrollments mentor crossPrice courseLevel courseThumbnail duration reviews video_num',
         )
-        .populate('review')
+        .populate('reviews')
         .lean();
     } catch (e) {
       throw new InternalServerErrorException(e);
