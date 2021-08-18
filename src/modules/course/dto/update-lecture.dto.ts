675036c01f48686de4ac0a5c2846576bcc7cb7a1
Devesh <rockdev1234560@gmail.com>
2021-08-18 15:04:54 +0530

    add isoptional

--- src/modules/course/dto/update-lecture.dto.ts
+++ src/modules/course/dto/update-lecture.dto.ts
@@ -1,36 +1,32 @@
-import { IsNotEmpty, IsString, IsUrl } from 'class-validator';
+import { IsString, IsUrl } from 'class-validator';
 
 export class UpdateLectureDto {
   /**
    * name of the lecture
    * @example "Play with pandas"
    */
-  @IsNotEmpty()
   @IsString()
-  lectureName: string;
+  lectureName?: string;
 
   /**
    * The description of the lecture in the schedule
    * @example "The pandas library"
    */
-  @IsNotEmpty()
   @IsString()
-  description: string;
+  description?: string;
 
   /**
    * The length of the video
    * @example "5 minutes"
    */
-  @IsNotEmpty()
   @IsString()
-  time: string;
+  time?: string;
 
   /**
    * The url video
    * @example "https://codeforcause.org/video/1"
    */
-  @IsNotEmpty()
   @IsString()
   @IsUrl()
-  lectureVideoUrl: string;
+  lectureVideoUrl?: string;
 }
